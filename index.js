09dd62bd27835a208ecdb0fb0ae31a5aee58e95c
Muhammad Haikal Al Rasyid <haikalslipi@gmail.com>
2022-01-25 08:21:42 +0000

    monyerah

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
-const mySecret = process.env['TOKEN']
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
+const token = process.env['token']
+const CLIENT_ID = process.env['client_id']
+const GUILD_ID = process.env['guild_id']
 
-client.on("message", msg => {
-    if(msg.content === "ping") {
-        msg.reply("pong")
-    }
-})
+const commands = [{
+  name: 'anj',
+  description: 'Replies with Heh!'
+}, {
+  name: 'Heh',
+  description: 'Replies with Gabole gitu!'
+}]; 
 
-client.login(mySecret)
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
